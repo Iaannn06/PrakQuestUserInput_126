3d49ad0701e69f81fe2b592ce9d4fc7afbb0e5a4
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-10-31 17:44:00 +0700

    membuat function RegistrationScreen dengan variabel nama, alamat, jenis kelamin, dan status

--- app/src/main/java/com/example/praktikumpamweek5/Praktikum5.kt
+++ app/src/main/java/com/example/praktikumpamweek5/Praktikum5.kt
@@ -1,2 +1,14 @@
 package com.example.praktikumpamweek5
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+@Composable
+fun RegistrationScreen() {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var selectedGender by remember { mutableStateOf("") }
+    var selectedStatus by remember { mutableStateOf("") }
+
+}
