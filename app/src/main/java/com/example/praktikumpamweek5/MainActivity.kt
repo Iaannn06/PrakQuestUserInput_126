7d07818dc154f655b82972be7786ba373cdf3f89
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-01 10:17:34 +0700

    finish commit dan penyesuaian code

--- app/src/main/java/com/example/praktikumpamweek5/MainActivity.kt
+++ app/src/main/java/com/example/praktikumpamweek5/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.example.praktikumpamweek5.ui.theme.PraktikumPamWeek5Theme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
